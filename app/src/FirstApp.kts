c09760d25c6d437cce57d40580517dfcc9455ccf
Atuoha <atuohainitiatives@gmail.com>
2022-10-17 06:55:14 +0100

    startin up

---
+++ app/src/FirstApp.kts
@@ -0,0 +1,9 @@
+fun main() {
+    print("helllllllllllllllllllllllllllllldkfjsdklfjsdklfjsdlkfj")
+    var student =  readLine()
+    print(student)
+    var name = "Tony"
+    println(name)
+//    android.util.Log.i("Hello")
+    print("Welcome to Kotlin bro!")
+}
